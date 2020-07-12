c95589dc04aa67fd4ad3328c18d374c20fd9d569
Vinit Patil <vinitpatil647@gmail.com>
2020-07-13 00:39:02 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/CropFrontend/index.html'));
+res.sendFile(path.join(__dirname,'/dist/CropFrontend/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
