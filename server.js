cc8ea9861baa2d55402eda04af582d8dd662eb28
Vinit Patil <vinitpatil647@gmail.com>
2020-07-13 00:31:57 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/dist'));
  
 app.get('/*', function(req,res) {
  
